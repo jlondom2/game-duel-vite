c23b3a9b6779f16c8794f7208a55ea4e1e17758a
jlondom2 <jlondom@gmail.com>
2023-01-20 20:40:44 -0500

    Update Right.jsx

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -11,6 +11,7 @@ export default function Right() {
   return (
     <div className="section-title-wrap blue">
       <div className="widget-sidebar">
+        <p>testing</p>
         <h2 className="section-title medium">Popular Posts</h2>
         <div className="section-title-separator"></div>
       </div>
